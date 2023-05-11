a1f9ccc11e4fd8a44efb282cacfaff6a35ff8b26
Rajin <42791346+Razeen1@users.noreply.github.com>
2023-05-11 10:10:21 +0545

    Update ProductCard.js
    Changes to product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -22,29 +22,47 @@ const ProductCard = () => {
     fetchProduct();
   }, []);
   return (<>
-    
-    <div class="flex flex-wrap px-20 py-10">
-      {products.map((values) => {
-        return (
-          <div class="w-1/4 px-2 py-5">
 
-                <div class="max-w-sm rounded overflow-hidden shadow-lg h-full">
-                    <img class="object-scale-down h-48 w-96" src={values.image} alt="pRODUCTS" />
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">{values.title}
-                            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 ">{values.rating.rate}</span></div>
+    <div class="flex flex-wrap px-20 py-10 ">
+     
+
+          <div className="ml-20 mr-20">
+            {products.map((product) => {
+              return (
+                <div className="inline-flex p-4" key={product.id}>
+                  <div className="mt-10 max-w-xs rounded overflow-hidden shadow-lg object-center text-center">
+                    <img
+                      className="rounded-t-lg p-8 object-scale-down h-48 w-96"
+                      src={product.image}
+                      alt="Sunset in the mountains"
+                    />
+
+                    <div className="px-4 py-2">
+                      <div className="font-bold text-xl mb-2 truncate">
+                        {product.title}
+                      </div>
+                      <p className="text-gray-700 text-base truncate ...">
+                        {product.description}
+                      </p>
+                      <p className="text-gray-900 text-base capitalize">
+                        {product.category}
+                      </p>
+                      <p className="font-bold">${product.price}</p>
+                      <div className=" p-3 text-center">
+                        <Link to={`/products/${product.id}`}>
+                          <button className="bg-blue-950 hover:bg-blue-700 text-white rounded-lg font-normal p-[5px]">
+                            Order Now
+                          </button>
+                        </Link>
+                      </div>
                     </div>
-                    <div class="px-5 pt-4 pb-2">
-                        <div class="flex items-center justify-between">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-black">${values.price}</span>
-                            <Link to={`/products/${values.id}`}><button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Detail</button></Link>
-                        </div>
-                    </div>
+                  </div>
                 </div>
-
-            </div>
-        );
-      })}
+              );
+            })}
+          </div>
+        
+      
     </div></>
   );
 };
